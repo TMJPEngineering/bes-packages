b9da940f9fbe47b774c7828df1d6adc8d748b8ab
John Conrad Lumacad <jcmlumacad@tmj.ph>
2018-03-13 16:46:35 +0800

    Added csurf

--- bes-middleware/src/Middleware/BaseVerifier.js
+++ bes-middleware/src/Middleware/BaseVerifier.js
@@ -1,12 +1,19 @@
+var csurf = require('csurf');
+
 var BaseVerifier = function (except, request, response, next) {
     var url = request.originalUrl;
 
     if (except.includes(url)) {
-        response.clearCookie('_csrf');
-        response.clearCookie('XSRF-TOKEN');
+        return next();
     }
 
-    return next();
+    var csrf = csurf({ cookie: true });
+    csrf(request, response, next);
+
+    return (request, response, next) => {
+        response.cookie('XSRF-TOKEN', request.csrfToken());
+        next();
+    }
 };
 
 module.exports = BaseVerifier;
